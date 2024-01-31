1288ba4c10c24d2e5c83a860dc174dd954f55f4e
pravinkuyate <pravinkuyate0@gmail.com>
2024-01-31 13:20:00 +0530

    commit by pravin

--- test_XGboost.py
+++ test_XGboost.py
@@ -4,7 +4,7 @@ from sklearn.model_selection import KFold
 from sklearn.metrics import accuracy_score
 from xgboost import XGBClassifier
 from sklearn.model_selection import cross_val_score
-
+from sklearn.model_selection import train_test_split
 
 data=pd.read_csv(r"C:\Users\pravinkuyate\OneDrive\Desktop\DS and ALGO\Wholesale customers data.csv")
 data.head()
@@ -15,9 +15,12 @@ y = data['Channel']
 
 y = y.map({1: 0, 2: 1})
 
-
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 model = XGBClassifier()
+model.fit(X_train, y_train)
 
+
+y_pred = model.predict(X_test)
 
 accuracy_scores = cross_val_score(model, X, y, cv=5)
 
